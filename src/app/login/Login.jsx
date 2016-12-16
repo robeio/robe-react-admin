b33fe7dad87db1e2c6c63e352d5482dcc4dcbac0
serayuzgur <serayuzgur@gmail.com>
2016-12-16 15:03:15 +0300

    login

--- src/app/login/Login.jsx
+++ src/app/login/Login.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import ShallowComponent from "robe-react-commons/lib/components/ShallowComponent";
+import { ShallowComponent, AjaxRequest } from "robe-react-commons";
 import { Form, Row, Alert, Image, Button, InputGroup } from "react-bootstrap";
 import TextInput from "robe-react-ui/lib/inputs/TextInput";
 import PasswordInput from "robe-react-ui/lib/inputs/PasswordInput";
 import FaIcon from "robe-react-ui/lib/faicon/FaIcon";
+import SHA256 from "crypto-js/sha256";
 
 import cookie from "react-cookie";
 import Card from "libs/card/Card";
 
 class Login extends ShallowComponent {
+
+    loginPost = new AjaxRequest({
+        url: "authentication/login",
+        type: "POST"
+    });
 
     constructor(props: Object) {
         super(props);
@@ -24,8 +30,8 @@ class Login extends ShallowComponent {
             <div className="center-block"
                 style={{ maxWidth: 300 }}>
                 <Card>
-                    <Form method="post" action="http://127.0.0.1:8081/robe/authentication">
-                        <Row style={{textAlign:"center", marginBottom:"30px"}}>
+                    <Form>
+                        <Row style={{ textAlign: "center", marginBottom: "30px" }}>
                             <Image src="./logo.png" circle width="150" />
                         </Row>
                         <Row>
@@ -38,6 +44,7 @@ class Login extends ShallowComponent {
                                 onChange={this.__handleChange}
                                 inputGroupLeft={<InputGroup.Addon><FaIcon code="fa-user" /></InputGroup.Addon>}
                                 validationDisplay="overlay"
+                                onKeyPress={this.__onKeyPress}
                                 />
                         </Row>
                         <Row>
@@ -49,14 +56,14 @@ class Login extends ShallowComponent {
                                 onChange={this.__handleChange}
                                 inputGroupLeft={<InputGroup.Addon><FaIcon code="fa-lock" /></InputGroup.Addon>}
                                 placeholder="Password"
+                                onKeyPress={this.__onKeyPress}
                                 />
                         </Row>
                         <Row>
                             <Button
                                 className="btn btn-primary btn-login btn-block"
-                                type="submit"
                                 ref="submitBtn"
-                                onClick={this.handleSubmit}
+                                onClick={this.__handleSubmit}
                                 >
                                 Login
                             </Button>
@@ -81,11 +88,73 @@ class Login extends ShallowComponent {
         return true;
     }
 
-    handleSubmit() {
-        // cookie.save("domain", "http://0.0.0.1:8080/", { path: "/" });
-        // cookie.save("username", this.state.username.trim(), { path: "/" });
-        // cookie.save("password", this.state.password.trim(), { path: "/" });
-        // location.reload();
+    __handleSubmit() {
+        cookie.remove("auth-token");
+        var username = this.state.username.trim();
+        var password = this.state.password.trim();
+
+        if (!password || !username) {
+            return;
+        }
+
+        let data = {
+            username: username,
+            password: SHA256(password).toString()
+        };
+
+
+        this.loginPost.call(data, undefined, this.__loginSuccess, this.__loginError);
+    }
+
+    __loginSuccess(response, textStatus, xhr) {
+
+        var domain = response.domain;
+        var params = domain.split(';');
+
+        var path = "";
+
+        for (var i in params) {
+            var param = params[i];
+            if (param.indexOf("path") == 0) {
+                path = param.split("=")[1];
+            }
+            if (param.indexOf("domain") == 0) {
+                domain = param.split("=")[1];
+            }
+        }
+        cookie.save('domain', domain, { path: path });
+        cookie.save('username', response.email, { path: path });
+
+        location.reload();
+    }
+
+    __loginError(response: Object) {
+        var text = response.responseText;
+        switch (response.status) {
+            case 200:
+                text = "";
+                break;
+            case 401:
+                text = "Kullanıcı adı ya da şifre hatalı girdiniz.";
+                break;
+            case 500:
+                if (text == "User blocked.") {
+                    text = "Çok fazla hatalı giriş yaptınız.";
+                }
+                else {
+                    text = "Kullanıcı adı ya da şifre hatalı girdiniz.";
+                }
+                break;
+            default:
+                text = "Lütfen daha sonra tekrar deneyiniz.";
+                break;
+        }
+        NotificationManager.error(text);
+    }
+
+    __onKeyPress(e) {
+        if (e.key == "Enter")
+            this.__handleSubmit();
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {render} from "react-dom";
+import { render } from "react-dom";
 import Switch from "app/Switch";// eslint-disable-line import/no-unresolved
+import Application from "robe-react-commons/lib/application/Application";
 
 const app = document.getElementById("app");
 
@@ -8,7 +9,10 @@ const app = document.getElementById("app");
 // window.applicationRootPath = "/robe/admin-ui/";
 
 // ****FOR DEV****
-window.applicationRootPath = "/";
+// window.applicationRootPath = "/";
+
+
+Application.setBaseUrlPath("http://127.0.0.1:8081/robe");
 
 
 render((<Switch />), app);
